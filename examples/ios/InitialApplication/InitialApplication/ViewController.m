feb6f72907ddbe0cb7841de204f54ba61d4e32f8
OlehKulykov <info@resident.name>
2013-07-04 03:15:15 +0300

    JSON integration

---
+++ examples/ios/InitialApplication/InitialApplication/ViewController.m
@@ -0,0 +1,80 @@
+
+#import "ViewController.h"
+#import "MyGLView.h"
+
+@interface ViewController () {
+   
+}
+
+@property (strong, nonatomic) EAGLContext *context;
+
+
+- (void)setupGL;
+- (void)tearDownGL;
+
+@end
+
+@implementation ViewController
+
+- (void)dealloc
+{
+    [_context release];
+    [super dealloc];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.context = [[[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES1] autorelease];
+
+    if (!self.context)
+	{
+        NSLog(@"Failed to create ES context");
+    }
+    
+    MyGLView * view = (MyGLView *)self.view;
+    view.context = self.context;
+    view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
+    
+    [self setupGL];
+}
+
+- (void)viewDidUnload
+{    
+    [super viewDidUnload];
+    
+    [self tearDownGL];
+    
+    if ([EAGLContext currentContext] == self.context)
+	{
+        [EAGLContext setCurrentContext:nil];
+    }
+    self.context = nil;
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone)
+	{
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    }
+	else
+	{
+        return YES;
+    }
+}
+
+- (void)setupGL
+{
+    [EAGLContext setCurrentContext:self.context];
+}
+
+- (void)tearDownGL
+{
+    [EAGLContext setCurrentContext:self.context];
+    
+}
+
+@end
